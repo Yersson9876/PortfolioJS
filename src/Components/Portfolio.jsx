39feb346beb878c58e83a27ce72636668d7e5ea9
Yersson9876 <81572818+Yersson9876@users.noreply.github.com>
2024-03-02 18:21:03 +0000

    New Homepage

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/desk.jpeg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "desktop with toys reference Mario and Fortnite";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "🐈El Gran Rescate - Video Game Unity DEMO🎮",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Unity-created video game in Beat-em-Up style featuring a cat as the protagonist",
+    url: "https://github.com/Yersson9876/granRescate",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Web Page for Startup",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Website for a business, created using HTML5, CSS, and Bootstrap, demonstrating the use of a MySQL database.",
+    url: "https://nxlevel.byethost15.com/",
   },
   {
-    title: "My Resume Site",
+    title: "Talk on 3D Graphic Environments",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Practical talk on how 3D graphic environments, simulating real-world spaces, are created starting from a square. Exploring how this concept has played a crucial role in video games, leading to virtual realities and simulators.",
+    url: "https://www.facebook.com/r00thouse/posts/pfbid0wcX2pCWdVnZoYoiVfhQpLUWj5qpbKRYghF9mxqrrs7CoTk5vyYnGS8SoZ374oHUMl",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Game Page",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "I broadcast and chat about video games, covering platforms ranging from Nintendo and PlayStation to PC.",
+    url: "https://www.twitch.tv/yersson9877",
   },
 ];
 
